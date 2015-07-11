89dc41a755218416d2db06c6476d3aa7d7ab359b
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-07-10 22:50:11 -0600

    refactoring

--- test/models/task_manager_test.rb
+++ test/models/task_manager_test.rb
@@ -1,38 +1,35 @@
 require_relative '../test_helper'
 
 class TaskManagerTest < Minitest::Test
+
   def test_it_creates_a_task
-    TaskManager.create({ :title       => "a title",
-                         :description => "a description"})
+    2.times do
+      create_task
+    end
+
     task = TaskManager.find(TaskManager.all.first.id)
+
     assert_equal "a title", task.title
     assert_equal "a description", task.description
-    assert_equal TaskManager.all.first.id, task.id
+    assert_equal task.id, task_id
   end
 
   def test_it_finds_all_tasks
-    TaskManager.create({ :title       => "a title",
-                         :description => "a description"})
-
-    TaskManager.create({ :title       => "a second title",
-                         :description => "a second description"})
-
-    assert_equal 2, TaskManager.all.count
+    2.times do
+      create_task
+    end
+    assert_equal 2, total_tasks
   end
 
   def test_it_finds_a_task_by_id
-    TaskManager.create({ :title       => "a title",
-                         :description => "a description"})
-
-    TaskManager.create({ :title       => "a second title",
-                         :description => "a second description"})
-
-    assert_equal "a title", TaskManager.find(TaskManager.all.first.id).title
+    2.times do
+      create_task
+    end
+    assert_equal "a title", TaskManager.find(task_id).title
   end
 
   def test_it_updates_a_task
-    task = TaskManager.create({ :title       => "a title",
-                                :description => "a description"})
+    task = create_task
 
     TaskManager.update(task.id, {title: "new title", description: "new desc"})
 
@@ -42,13 +39,24 @@ class TaskManagerTest < Minitest::Test
 
   def test_it_deletes_a_task
     2.times do
-      TaskManager.create({ :title       => "a title",
-                           :description => "a description"})
+      create_task
     end
 
-    total = TaskManager.all.count
-    id = TaskManager.all.first.id
-    TaskManager.delete(id)
-    assert_equal (total - 1), TaskManager.all.count
+    total = total_tasks
+    TaskManager.delete(task_id)
+    assert_equal (total - 1), total_tasks
+  end
+
+  def create_task
+    TaskManager.create({ :title       => "a title",
+                          :description => "a description"})
+  end
+
+  def total_tasks
+    TaskManager.all.count
+  end
+
+  def task_id
+    TaskManager.all.first.id
   end
 end
